71ca84ff6520e0b2ea109d86703b5099d678caed
Nityananda Barbosa <nityanandabarbosa@gmail.com>
2022-04-24 16:48:40 -0300

    feat :find and delete a aluno -  class 6

---
+++ src/main/java/class_6/ClientDeleteAluno.java
@@ -0,0 +1,16 @@
+package class_6;
+
+import javax.persistence.EntityManager;
+
+import class_5.Aluno;
+import utils.JpaUtils;
+
+public class ClientDeleteAluno {
+	public static void main(String[] args) {
+		EntityManager manager = JpaUtils.getEntityManager();
+		manager.getTransaction().begin();
+		Aluno aluno = manager.find(Aluno.class, 1L);
+		manager.remove(aluno);
+		manager.getTransaction().commit();
+	}
+}
