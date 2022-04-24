8aa25e304c19f7b09cec87dff044d93afbefe133
Nityananda Barbosa <nityanandabarbosa@gmail.com>
2022-04-24 16:52:25 -0300

    feat : update a aluno -  class 6

---
+++ src/main/java/class_6/ClientUpdateAluno.java
@@ -0,0 +1,18 @@
+package class_6;
+
+import javax.persistence.EntityManager;
+
+import class_5.Aluno;
+import utils.JpaUtils;
+
+public class ClientUpdateAluno {
+	public static void main(String[] args) {
+		EntityManager manager = JpaUtils.getEntityManager();
+		manager.getTransaction().begin();
+		Aluno aluno = manager.find(Aluno.class, 3L);
+		aluno.setBairro("Parangaba");
+		aluno.setEndereco("Rua Dom Pedro II, 10");
+		manager.persist(aluno);
+		manager.getTransaction().commit();
+	}
+}
